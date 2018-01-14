0f4d83ec6b9a1e05ac560285cb3c6b4a09f04e09
luhaiping <315979942@qq.com>
2018-01-14 19:13:22 +0800

    新版微官网

---
+++ mobilestatic/js/menu.js
@@ -0,0 +1,28 @@
+$(".menu").attr("isopen","off");
+$(".menu").click(function(){
+    var isopen=$(this).attr("isopen");
+    if(isopen=="off"){
+        $(".menuleft").css({
+            "width":"3.75rem"
+        })
+        $(".menuright").css({
+            left:"3.75rem"
+        })
+        $(".menu-s-trans1,.menu-s-trans2,.menu-s-trans3,.menu-s-trans4").css({
+            "margin-left":"0"
+        });
+        $(this).attr("isopen","on")
+    }else{
+        $(".menuleft").css({
+            "width":"0"
+        })
+        $(".menuright").css({
+            left:"0"
+        })
+        $(".menu-s-trans1,.menu-s-trans2,.menu-s-trans3,.menu-s-trans4").css({
+            "margin-left":"-3.75rem"
+        });
+        $(this).attr("isopen","off")
+    }
+
+})
