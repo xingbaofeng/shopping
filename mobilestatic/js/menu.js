129944b53d4ab62b6d44e16c66db8499f1d97ded
luhaiping <315979942@qq.com>
2018-01-17 21:24:50 +0800

    tiaozheng

--- mobilestatic/js/menu.js
+++ mobilestatic/js/menu.js
@@ -8,7 +8,7 @@ $(".menu").click(function(){
         $(".menuright").css({
             left:"3.75rem"
         })
-        $(".menu-s-trans1,.menu-s-trans2,.menu-s-trans3,.menu-s-trans4").css({
+        $(".menu-s-trans1,.menu-s-trans2,.menu-s-trans3,.menu-s-trans4,.menu-s-trans5").css({
             "margin-left":"0"
         });
         $(this).attr("isopen","on")
@@ -19,7 +19,7 @@ $(".menu").click(function(){
         $(".menuright").css({
             left:"0"
         })
-        $(".menu-s-trans1,.menu-s-trans2,.menu-s-trans3,.menu-s-trans4").css({
+        $(".menu-s-trans1,.menu-s-trans2,.menu-s-trans3,.menu-s-trans4,.menu-s-trans5").css({
             "margin-left":"-3.75rem"
         });
         $(this).attr("isopen","off")
